d1936829b26fa7889719e6cecea9b7d3dc3779b4
fomith <55940138+fomith@users.noreply.github.com>
2025-01-28 16:52:56 +0300

    Update WelcomePage.view.tsx

--- src/pages/WelcomePage/WelcomePage.view.tsx
+++ src/pages/WelcomePage/WelcomePage.view.tsx
@@ -123,7 +123,7 @@ const WelcomePageView = () => {
           alignItems="center"
         >
           <MessengerButton
-            href="https://t.me/m/na8mrlRkNjYy"
+            href="https://t.me/m/ZCJbSY39OTVi"
             text="Написать в Telegram"
             iconSrc={telegramIcon}
             backgroundColor="#77c6e5"
@@ -132,7 +132,7 @@ const WelcomePageView = () => {
           />
 
           <MessengerButton
-            href="https://wa.me/79315878205?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%2C%20%D0%BF%D0%B8%D1%88%D1%83%20%D1%87%D1%82%D0%BE%D0%B1%D1%8B%20%D0%B7%D0%B0%D0%B1%D1%80%D0%B0%D1%82%D1%8C%20%D0%BA%D1%83%D0%BF%D0%BE%D0%BD%2010000%20%D1%80%D1%83%D0%B1%20%D0%BD%D0%B0%20%D0%BB%D0%B0%D0%B7%D0%B5%D1%80%D0%BD%D1%83%D1%8E%20%D1%8D%D0%BF%D0%B8%D0%BB%D1%8F%D1%86%D0%B8%D1%8E!"
+            href="https://wa.me/79315878205?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%2C%20%D1%8F%20%D0%BF%D0%B8%D1%88%D1%83%20%D0%B4%D0%BB%D1%8F%20%D1%82%D0%BE%D0%B3%D0%BE%20%D1%87%D1%82%D0%BE%D0%B1%D1%8B%20%D0%B7%D0%B0%D0%B1%D1%80%D0%B0%D1%82%D1%8C%20%D0%BA%D1%83%D0%BF%D0%BE%D0%BD%2010000%20%D1%80%D1%83%D0%B1%20%D0%BD%D0%B0%20%D0%BB%D0%B0%D0%B7%D0%B5%D1%80%D0%BD%D1%83%D1%8E%20%D1%8D%D0%BF%D0%B8%D0%BB%D1%8F%D1%86%D0%B8%D1%8E"
             text="Написать в WhatsApp"
             iconSrc={whatsAppIcon}
             backgroundColor="#2cb742"
